3b1aba02d7d53f7b2a2e52baedff0288f1066edd
Andre Lina <528289176@qq.com>
2023-02-22 23:30:34 +0800

    add AnimatedSticker component

---
+++ src/components/AnimatedSticker.vue
@@ -0,0 +1,113 @@
+<template>
+  <div ref="containerRLottie" class="test"></div>
+</template>
+
+<script setup lang="ts">
+import generateIdFor from '@/util/generateIdFor';
+import {onMounted, reactive, ref} from "vue";
+
+export type OwnProps = {
+  animationId?: string;
+  className?: string;
+  style?: string;
+  tgsUrl: string;
+  play?: boolean | string;
+  playSegment?: [number, number];
+  speed?: number;
+  noLoop?: boolean;
+  size: number;
+  quality?: number;
+  color?: [number, number, number];
+  isLowPriority?: boolean;
+  forceOnHeavyAnimation?: boolean;
+  sharedCanvas?: HTMLCanvasElement;
+  sharedCanvasCoords?: { x: number; y: number };
+  onClick?: NoneToVoidFunction;
+  onLoad?: NoneToVoidFunction;
+  onEnded?: NoneToVoidFunction;
+  onLoop?: NoneToVoidFunction;
+};
+type RLottieInstance = import('@/lib/rlottie/RLottie').default;
+type RLottieClass = typeof import('@/lib/rlottie/RLottie').default;
+let lottiePromise: Promise<RLottieClass>;
+let RLottie: RLottieClass;
+let container: HTMLDivElement
+
+// Time for the main interface to completely load
+const LOTTIE_LOAD_DELAY = 3000;
+const ID_STORE = {};
+
+async function ensureLottie() {
+  if (!lottiePromise) {
+    lottiePromise = import('@/lib/rlottie/RLottie') as unknown as Promise<RLottieClass>;
+    RLottie = (await lottiePromise as any).default;
+  }
+
+  return lottiePromise;
+}
+// setTimeout(ensureLottie, LOTTIE_LOAD_DELAY);
+// await ensureLottie();
+const containerId = () => generateIdFor(ID_STORE, true);
+
+const prop = defineProps<OwnProps>();
+
+let containerRLottie = ref<any>(null);
+
+onMounted(() => {
+  container = containerRLottie.value;
+  init(prop);
+});
+
+const init = async ({
+   animationId,
+   className,
+   style,
+   tgsUrl,
+   play,
+   playSegment,
+   speed,
+   noLoop,
+   size,
+   quality,
+   isLowPriority,
+   color,
+   forceOnHeavyAnimation,
+   sharedCanvas,
+   sharedCanvasCoords,
+   onClick,
+   onLoad,
+   onEnded,
+   onLoop,
+  }: OwnProps
+) => {
+  if (!container) {
+    return;
+  }
+  await ensureLottie();
+  const newAnimation = RLottie.init(
+    "containerId",
+    container,
+    onLoad,
+    animationId || generateIdFor(ID_STORE, true),
+    tgsUrl,
+    {
+      noLoop,
+      size,
+      quality,
+      isLowPriority,
+      coords: sharedCanvasCoords,
+    },
+    color,
+    onEnded,
+    onLoop,
+  );
+  if (speed) {
+    newAnimation.setSpeed(speed);
+  }
+  newAnimation.play();
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="container">
-    <h1>Lottie-Web</h1>
+    <h1>Lottie-Web Demo</h1>
+    <div class="lottie">
+      <animated-sticker
+        v-for="i in LOCAL_TGS_URLS"
+        :size=180
+        :tgs-url="i"
+        :playSegment=SEGMENT_COVER_EYES
+        :noLoop="false"/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import AnimatedSticker from "@/components/AnimatedSticker.vue";
+import { LOCAL_TGS_URLS } from '@/assets/animatedAssets';
 
+const SEGMENT_COVER_EYES: [number, number] = [0, 50];
 
 </script>
 
 <style scoped>
 .container {
   text-align: center;
+}
+
+.lottie {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 70%;
+  margin: auto;
+}
+
+.lottie * {
+  padding: 30px;
 }
 </style>
